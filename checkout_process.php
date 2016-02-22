8445ce3a9ed99b58ad5a6aab82875680885da51f
Ben Laudicano <blaudicano@linchpinagency.com>
2016-02-22 14:30:28 -0500

    Fixed up part 2 step 7

--- checkout_process.php
+++ checkout_process.php
@@ -7,10 +7,25 @@ $city = mysqli_real_escape_string($connection,$_POST["city"]);
 $zip = mysqli_real_escape_string($connection,$_POST["zip"]);
 
 if ($firstname == "") {
-  $errormessage = $errormessage . "First name must be more than one character";
+    $errormessage =  $errormessage . "First Name was blank <br />";
 }
 
+if ($address == "") {
+    $errormessage =  $errormessage . "Address was blank <br />";
+}
 
+if ($city == "") {
+    $errormessage =  $errormessage . "City was blank <br />";
+}
+
+if ($zip == "") {
+    $errormessage =  $errormessage . "Zip was blank <br />";
+}
+
+if ($errormessage != "") {
+    include("checkout_form.php");
+    die();
+}
 
    echo "order placed.";
 
